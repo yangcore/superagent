7c5867fc3c0e33323b9d827ff51de2bdd67ac508
yangchao <yangchao03@paicaifu.com>
2017-08-23 23:32:11 +0800

    更新3.0

--- index.js
+++ index.js
@@ -10,78 +10,59 @@ var url = {
     target_url: "http://invest.ppdai.com/loan/info",
     singleApply_url: "http://invest.ppdai.com/Negotiable/SingleApplyNew"
 };
-
-var log4js = require('log4js');
-var log4js_config = require("./log4.json");
-log4js.configure(log4js_config);
-var LogFile_warn = log4js.getLogger('log_file_warn');
-var LogFile_suc = log4js.getLogger('log_file_suc');
-var LogFile_err = log4js.getLogger('log_file_err');
-
-
 // //访问登录接口获取cookie
-module.exports = function (userid, pwd, tobepaid, priceForSaleRate, xyz, levle, idJson) {
-    var idJson = eval(idJson);
-    return new Promise((resolve, reject) => {
-        superagent.post(url.login_url)
-            .type("form")
+module.exports ={
+    getcookie:function  (userid, pwd) {
+        return new Promise((resolve, reject) => {
+            superagent.post(url.login_url)
+                .type("form")
+                .set(browserMsg)
+                .send({
+                    UserName: userid,
+                    Password: pwd,
+                    IsAsync: true,
+                    Redirect: "",
+                    RememberMe: false
+                })
+                .end(function (err, res) {
+                    var stu;
+                    if (err) {
+                        console.error(err + '   获取cookie接口');
+                        LogFile_err.error(err + '   获取cookie接口');
+                    } 
+                    var cookie = res.header["set-cookie"];
+                    resolve(cookie);
+                })
+        })
+    },
+    getData:  function (cookie, id, tobepaid, priceForSaleRate, xyz, levle) {
+        return new Promise((resolve, reject) => {
+        superagent.get(url.target_url)
+            .set("Cookie", cookie)
             .set(browserMsg)
-            .send({
-                UserName: userid,
-                Password: pwd,
-                IsAsync: true,
-                Redirect: "",
-                RememberMe: false
-            })
+            .query(id)
             .end(function (err, res) {
-                var stu;
                 if (err) {
-                    console.error(err + '   获取cookie接口');
-                    LogFile_err.error(err + '   获取cookie接口');
-                } 
-                var cookie = res.header["set-cookie"];
-                if (cookie.length > 3) {
-                    cookie = cookie[0].split(';')[0] + ';' + cookie[1].split(';')[0] + ';' + cookie[2].split(';')[0] + ';' + cookie[3].split(';')[0] + ';' + cookie[4].split(';')[0] + ';' + cookie[5].split(';')[0] + ';';
-                    for (var i = 0; i < idJson.length; i++) {
-                        getData(cookie, idJson[i].id, tobepaid, priceForSaleRate, xyz, levle);
-                    }
-                    stu = true;
-                } else {
-                    stu = false
+                    console.error(err + '    获取页面数据接口');
+                    LogFile_err.error(err + '    获取页面数据接口');
+                };
+                if(res.text!==undefined){
+                    var $ = cheerio.load(res.text);
+                    var obj = {id};
+                    obj.id = id.id;
+                    obj.liabilities = $('.table-hd').eq(3).prev().children().eq(1).find("span").text().replace(/¥/g, '').replace(/,/g, '');//历史负债
+                    obj.tobepaid = $('.table-hd').eq(3).prev().prev().children().eq(0).find("span").text().replace(/¥/g, '').replace(/,/g, '');//代还金额
+                    obj.priceForSaleRate = priceForSaleRate;
+                    obj.levle = levle;
+                    resolve(obj)
+                }else{
+                    resolve(null);
                 }
-                resolve(stu);
-            })
-    })
-}
-
-
-
-function getData(cookie, id, tobepaid, priceForSaleRate, xyz, levle) {
-    superagent.get(url.target_url)
-        .set("Cookie", cookie)
-        .set(browserMsg)
-        .query({ id: id })
-        .end(function (err, res) {
-            if (err) {
-                console.error(err + '    获取页面数据接口');
-                LogFile_err.error(err + '    获取页面数据接口');
-            };
-            var $ = cheerio.load(res.text);
-            var obj = {};
-            obj.id = id;
-            obj.liabilities = $('.table-hd').eq(3).prev().children().eq(1).find("span").text().replace(/¥/g, '').replace(/,/g, '');//历史负债
-            obj.tobepaid = $('.table-hd').eq(3).prev().prev().children().eq(0).find("span").text().replace(/¥/g, '').replace(/,/g, '');//代还金额
-            obj.priceForSaleRate = priceForSaleRate;
-            obj.levle = levle;
-            console.info(obj.tobepaid , obj.liabilities , xyz , obj.tobepaid , tobepaid);
-            if (obj.tobepaid / obj.liabilities >= xyz || obj.tobepaid >= tobepaid) {
-                singleApply(obj);
-            }else{
-                LogFile_warn.warn("id  " + obj.id + "    不符合条件");
-            }
-        });
-
-    function singleApply(obj) {
+            });
+        })
+    },
+singleApply : function (obj) {
+        return new Promise((resolve, reject) => {
         superagent.post(url.singleApply_url)
             .set("Cookie", cookie)
             .set(browserMsg)
@@ -95,14 +76,15 @@ function getData(cookie, id, tobepaid, priceForSaleRate, xyz, levle) {
                     console.error(err + '    申请债券接口');
                     LogFile_err.error(err + '    申请债券接口');
                 }
-                console.info(res.body.Code);
-                if (res.body.Code == 1) {
-                    // console.info(obj.id + "    成功申请1条债权");
-                    LogFile_suc.info(obj.id + "    成功申请1条债权");
-                } else {
-                    // console.warn("id  " + obj.id + "    申请失败");
-                    LogFile_warn.warn("id  " + obj.id + "    申请失败");
-                }
-            });
+                resolve(res.body.Code);
+            })
+        })
     }
-}
+} 
+
+
+
+
+
+    
+
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var getinfo = require('../index');
+var getinfo = require('../index').getcookie;
+var getData = require('../index').getData;
+var singleApply = require('../index').singleApply;
 var co = require('co');
+var log4js = require('log4js');
+var log4js_config = require("../log4");
+log4js.configure(log4js_config);
+var LogFile_warn = log4js.getLogger('log_file_warn');
+var LogFile_suc = log4js.getLogger('log_file_suc');
+var LogFile_err = log4js.getLogger('log_file_err');
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index');
@@ -11,8 +19,37 @@ router.get('/', function (req, res, next) {
 router.post('/', function (req, res, next) {
   let gen = function* () {
     var reqbody = req.body;
-    var statu = yield getinfo(reqbody.user, reqbody.password, reqbody.tobepaid, reqbody.priceForSaleRate, reqbody.xyz, reqbody.levle, reqbody.idJson);
-    if (statu) {
+    var idJson = eval(reqbody.idJson);
+    var cookieInfo = yield getinfo(reqbody.user, reqbody.password);
+    if (cookieInfo.length > 3) {
+      cookieInfo = cookieInfo[0].split(';')[0] + ';' + cookieInfo[1].split(';')[0] + ';' + cookieInfo[2].split(';')[0] + ';' + cookieInfo[3].split(';')[0] + ';' + cookieInfo[4].split(';')[0] + ';' + cookieInfo[5].split(';')[0] + ';';
+      for (var i = 0; i < idJson.length; i++) {
+        var obj = yield getData(cookieInfo, idJson[i], reqbody.tobepaid, reqbody.priceForSaleRate, reqbody.xyz, reqbody.levle);
+        console.info(obj);
+        if (obj) {
+          if (obj.tobepaid / obj.liabilities >= reqbody.xyz || obj.tobepaid >= reqbody.tobepaid) {
+            var Code = yield singleApply(obj);
+            console.info(obj.id + "拍拍贷返回状态码" + Code);
+            if (Code == 1) {
+              console.info(obj.id + "    成功申请1条债权");
+              LogFile_suc.info(obj.id + "    成功申请1条债权");
+            } else if (Code == 0) {
+              console.warn("id  " + obj.id + "    申请失败");
+              LogFile_warn.warn("id  " + obj.id + "    申请失败");
+            } else {
+              console.warn("id  " + obj.id + "    返回数据错误");
+              LogFile_warn.warn("id  " + obj.id + "    返回数据错误");
+            }
+          } else {
+            console.info("id  " + obj.id + "    不符合条件");
+            LogFile_warn.warn("id  " + obj.id + "    不符合条件");
+          }
+        }else{
+          console.info("id  " + idJson[i].id + "    页面抓取失败");
+          LogFile_warn.warn("id  " + idJson[i].id + "    页面抓取失败");
+        }
+      }
+      console.info("申请结束");
       res.send({ code: '0000', msg: "申请完成,请查看logs文件夹下的日志" });
     } else {
       res.send({ code: '1001', msg: "账户名或密码也许不正确，请检查再试" });
@@ -24,24 +61,24 @@ router.post('/', function (req, res, next) {
 
 })
 var path = require('path')
-var rf=require("fs"); 
-router.post('/readInfo',function(req, res, next){
- if(req.body.type=="suc"){
-  var data=rf.readFileSync(path.resolve(__dirname, '..')+"/logs/logs_suc.log","utf-8");
- }else if(req.body.type=="fail"){
-  var data=rf.readFileSync(path.resolve(__dirname, '..')+"/logs/logs_warn.log","utf-8");
- }
+var rf = require("fs");
+router.post('/readInfo', function (req, res, next) {
+  if (req.body.type == "suc") {
+    var data = rf.readFileSync(path.resolve(__dirname, '..') + "/logs/logs_suc.log", "utf-8");
+  } else if (req.body.type == "fail") {
+    var data = rf.readFileSync(path.resolve(__dirname, '..') + "/logs/logs_warn.log", "utf-8");
+  }
   res.send(data);
 });
 
-router.post('/delnfo',function(req, res, next){
-  if(req.body.type=="suc"){
-    rf.writeFileSync(path.resolve(__dirname, '..')+"/logs/logs_suc.log","");
-  }else if(req.body.type=="fail"){
-    rf.writeFileSync(path.resolve(__dirname, '..')+"/logs/logs_warn.log","");
+router.post('/delnfo', function (req, res, next) {
+  if (req.body.type == "suc") {
+    rf.writeFileSync(path.resolve(__dirname, '..') + "/logs/logs_suc.log", "");
+  } else if (req.body.type == "fail") {
+    rf.writeFileSync(path.resolve(__dirname, '..') + "/logs/logs_warn.log", "");
   }
-   res.send({code:"0000",msg:"删除成功"});
- });
+  res.send({ code: "0000", msg: "删除成功" });
+});
 
 
 
